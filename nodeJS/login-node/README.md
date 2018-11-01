2b4011f6b614c38ceba360df13a09f2b9ead7524
tole22 <tole.maxi@hotmail.com>
2018-11-01 10:35:19 -0300

    end of app

--- nodeJS/login-node/README.md
+++ nodeJS/login-node/README.md
@@ -1,3 +1,7 @@
+Node.js Login/Register with MongoDB and Express complete with a profile page to show user data
+-Authentication passport
+
+
 -For start:
 npm start           it has a script variable in package.json for start
 
@@ -6,3 +10,5 @@ open in two different terminals :  the commands mongod  and mongo
 
 if dont have MongoDB installed, download and set the environment variables in path at mongDB/bin
 
+use nodeLogin     >>>> in the mongo console go to the BD
+show collections    >>>>show the tables in de database
--- nodeJS/login-node/src/app/routes.js
+++ nodeJS/login-node/src/app/routes.js
@@ -8,7 +8,12 @@ module.exports = (app, passport) => {
             message: req.flash('loginMessage')
         });
     });
-//    app.post('/login', passport.authenticate(''));
+    app.post('/login', passport.authenticate('local-login', {
+        successRedirect: '/profile',
+        failureRedirect: '/login',
+        failureFlash: true
+    }));
+
     
     app.get('/signup', (req, res) => {
         res.render('signup', {
@@ -21,9 +26,21 @@ module.exports = (app, passport) => {
         failureFlash: true
     }));
 
-    app.get('/profile', (req, res) => {
+    app.get('/profile', isLoggedIn, (req, res) => {
         res.render('profile', {
             user: req.user
         });
     });
+
+    app.get('/logout', (req, res) => {
+        req.logout();
+        res.redirect('/');
+    });
 };
+
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated()) {
+        return next();
+    }
+    return res.redirect('/');
+}
