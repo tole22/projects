88f0375e82e50bdfe7ffe4aac8d3ba414695079b
tole22 <tole.maxi@hotmail.com>
2018-12-03 11:04:36 -0300

    crud with messages ready, now go for register/signup

--- nodeJS/notes-app/src/routes/notes.js
+++ nodeJS/notes-app/src/routes/notes.js
@@ -27,12 +27,32 @@ router.post('/notes/new-note', async((req, res) => {
     } else {
         const newNote = new Note({ title, description});
         await(newNote.save());
+        req.flash('success_msg', 'Note Added Successfully');
         res.redirect('/notes');
     }
 }));
 
-router.get('/notes', (req, res) => {
-    res.send('Notes from database');
-});
+router.get('/notes', async((req, res) => {
+    
+    const notes = await(Note.find().sort({date: 'desc'}));
+    res.render('notes/all-notes', { notes });
+}));
+
+router.get('/notes/edit/:id', async((req, res) => {
+    const note = await(Note.findById(req.params.id));
+    res.render('notes/edit-note', {note});
+}));
+
+router.put('/notes/edit-note/:id', async((req, res) => {
+    const { title, description } = req.body;
+    await(Note.findByIdAndUpdate(req.params.id, { title, description }));
+    req.flash('success_msg', 'Note Updated Successfully');
+    res.redirect('/notes');
+}));
+router.delete('/notes/delete/:id', async((req, res) => {
+    await(Note.findByIdAndDelete(req.params.id));
+    req.flash('success_msg', 'Note Removed Successfully');
+    res.redirect('/notes');
+}));
 
 module.exports = router;
